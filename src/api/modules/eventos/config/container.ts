6afdbc191a34e8d6791f69a812a0da90e92acda2
Walmircsanto <walmirsantosrosa@gmail.com>
2024-10-22 22:52:16 -0300

    :sparkles: feat(container.ts): criar container de injeção de depencia

--- src/api/modules/eventos/config/container.ts
+++ src/api/modules/eventos/config/container.ts
@@ -2,14 +2,29 @@ import {container} from "tsyringe";
 
 import IEventoRepository from "../services/interfaces/IEventoRepository";
 import {EventoRepository} from "../typeorm/repositories/EventoRepository";
+import  EventoService from "../services/EventoService";
+import EventoController from "../controller/EventoController";
+import Evento from "../typeorm/entities/Evento";
 
 
 // o container e aonde vai realizar a nossa injeção de dependecia
 // o metodo registerSingleton me diz que ao inves de criar uma nova instancia toda vez que houver uma request, apenas utilize
 // a instancia ja existente.
-container.registerSingleton<IEventoRepository>( // indica que o tipo a ser registrado implementa a interface IEvento.  Essa interface define o contrato que a classe EventoRepository deve seguir
-    'EventoRepository',
-    EventoRepository
-)
+// container.register<EventoController>(EventoController, {useClass: EventoController});
+//
+// container.register<EventoService>(
+//     EventoService,
+//     {useClass: EventoService}
+// );
+//
+// container.register<IEventoRepository>( // indica que o tipo a ser registrado implementa a interface IEvento.  Essa interface define o contrato que a classe EventoRepository deve seguir
+//     EventoRepository,
+//     EventoRepository
+// )
 
-export default container
+container.registerSingleton(EventoRepository);
+container.registerSingleton(EventoService);
+container.registerSingleton(EventoController);
+container.resolve(EventoRepository);
+
+export  default container
