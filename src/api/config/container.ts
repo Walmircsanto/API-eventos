7304daeae71f58745987dcb6dfd2c889531c3ef4
Walmircsanto <walmirsantosrosa@gmail.com>
2024-11-07 15:59:13 -0300

    :bulb: docs(container.ts): add explanatory comments and new import

--- src/api/config/container.ts
+++ src/api/config/container.ts
@@ -1,13 +1,18 @@
 import {container} from "tsyringe";
 
-import EventoRepository from "../typeorm/repositories/EventoRepository";
-import {EventoService} from "../services/EventoService";
-import EventoController from "../controller/EventoController";
+import EventoRepository from "../modules/eventos/typeorm/repositories/EventoRepository";
+import {EventoService} from "../modules/eventos/services/EventoService";
+import EventoController from "../modules/eventos/controller/EventoController";
+import UsersRepository from "../modules/usuario/typeorm/repositories/UserRepository";
 
 
+//basicamente ta dizendo que quando precisar instancia ou usar uma instancia do eventoRepository e esse container que vai gerenciar
 container.registerSingleton(EventoRepository);
 container.registerSingleton(EventoService);
 container.registerSingleton(EventoController);
+
+container.register('IUserRepository',{useValue: UsersRepository})
+//para essa instancia ser utilizada e aqui que eu digo para a classe instanciaa
 container.resolve(EventoRepository);
 
 export  default container
