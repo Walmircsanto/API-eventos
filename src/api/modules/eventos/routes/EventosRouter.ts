464e982201633842b822e44b28144bd36adcb444
Walmircsanto <walmirsantosrosa@gmail.com>
2024-11-07 16:05:42 -0300

    :bulb: docs(EventosRoutes.ts): add comment that explains the container resolver

--- src/api/modules/eventos/routes/EventosRouter.ts
+++ src/api/modules/eventos/routes/EventosRouter.ts
@@ -1,11 +1,11 @@
 import {Router} from 'express';
 import EventoController from "../controller/EventoController";
-import container from "../config/container";
+import container from "../../../config/container";
 import multer from "multer";
 import {uploadImgEvento} from "../../../middleware/imageEvento";
 
 const eventosRouter = Router(); // Route e uma função dentro da express logo não instancio ela como uma classe
-const eventoController = container.resolve(EventoController);
+const eventoController = container.resolve(EventoController); //obter a instancia do eventoController
 
 eventosRouter.post("/", multer(uploadImgEvento.getConfig).single("file"), eventoController.createEvento.bind(eventoController));
 eventosRouter.get('/:id', eventoController.findById.bind(eventoController))
