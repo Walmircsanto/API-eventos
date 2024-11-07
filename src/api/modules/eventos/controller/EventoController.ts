8a2492163932d578469bf2879562e7d4582e6c75
Walmircsanto <walmirsantosrosa@gmail.com>
2024-11-07 16:02:44 -0300

    :recycle: refactor(EventoController.ts): remove the promise that adds the event name taken from the file

--- src/api/modules/eventos/controller/EventoController.ts
+++ src/api/modules/eventos/controller/EventoController.ts
@@ -1,84 +1,71 @@
-import 'reflect-metadata';
-import {EventoService} from "../services/EventoService";
-import {Request, Response} from 'express';
-import {inject, injectable} from "tsyringe";
+import "reflect-metadata";
+import { EventoService } from "../services/EventoService";
+import { Request, Response } from "express";
+import { inject, injectable } from "tsyringe";
 import EventoRequest from "../dto/EventoRequest";
-import {CONSTRAINT} from "sqlite3";
+import { CONSTRAINT } from "sqlite3";
 import AppError from "../../../shared/errors/AppError";
-
 
 @injectable()
 export default class EventoController {
-    constructor(@inject(EventoService) private readonly eventoService: EventoService) {
-    }
+  constructor(
+    @inject(EventoService) private readonly eventoService: EventoService
+  ) {}
 
+  // @ts-ignore
+  public async createEvento(req: Request, res: Response): Promise<Response | undefined> {
+    const {
+      titulo,
+      img,
+      status,
+      descricao,
+      dataInicio,
+      dataFim,
+      usuariosIds,
+      certificadoId,
+    } = req.body;
 
-    // @ts-ignore
-    public async createEvento(req: Request, res: Response): Promise<Response | undefined> {
-
-
-        const {
-            titulo,
-            img,
-            status,
-            descricao,
-            dataInicio,
-            dataFim,
-            usuariosIds,
-            certificadoId
-        } = req.body;
-
-        const event = await this.eventoService.createEvent(
-            {
-                titulo,
-                img,
-                status,
-                descricao,
-                dataInicio,
-                dataFim,
-                usuariosIds,
-                certificadoId
-            }).then(async (event) => {
-
-            const imgFileName = req.file?.filename;
-            if (imgFileName) {
-                console.log(imgFileName)
-                const id = event.id
-                const evento = await this.eventoService.createAvatarService({id, imgFileName})
-                return res.status(200).json(evento)
-            }
-        });
-
-
-    }
-
-
-    public async findById(req: Request, res: Response): Promise<Response | undefined> {
-
-        const id = parseInt(req.params.id)
-
-        // const eventoService = container.resolve(EventoService)
-        const event = await this.eventoService.findById({id});
-
+    const fileName = req.file?.filename;
+    const event = await this.eventoService
+      .createEvent({
+        titulo,
+        img: fileName || img,
+        status,
+        descricao,
+        dataInicio,
+        dataFim,
+        usuariosIds,
+        certificadoId,
+      })
+      .then((event) => {
         return res.status(200).json(event);
-    }
+      });
+  }
 
+  public async findById(
+    req: Request,
+    res: Response
+  ): Promise<Response | undefined> {
+    const id = parseInt(req.params.id);
 
-    public async findAll(req: Request, res: Response) {
+    // const eventoService = container.resolve(EventoService)
+    const event = await this.eventoService.findById({ id });
 
-        // const eventoService = container.resolve(EventoService);
-        console.log("CHAMOU EVENTOSERVICE")
-        const eventos = await this.eventoService.findAllEventos();
+    return res.status(200).json(event);
+  }
 
-        return res.status(200).json(eventos);
-    }
+  public async findAll(req: Request, res: Response) {
+    // const eventoService = container.resolve(EventoService);
+    console.log("CHAMOU EVENTOSERVICE");
+    const eventos = await this.eventoService.findAllEventos();
 
-    public async deleteEvento(req: Request, res: Response){
-        const id = parseInt(req.params.id)
-         await this.eventoService.deleteEvento(id);
+    return res.status(200).json(eventos);
+  }
 
-        return res.status(204).json('');
-    }
+  public async deleteEvento(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+    await this.eventoService.deleteEvento(id);
 
-
+    return res.status(204).json("");
+  }
 }
