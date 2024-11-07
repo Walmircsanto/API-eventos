2c02231b4b4ead62995e297154741d1ce7136a9e
Walmircsanto <walmirsantosrosa@gmail.com>
2024-11-07 16:21:01 -0300

    :sparkles: feat(IUserRepository.ts): create interface and implement methods

--- src/api/modules/usuario/typeorm/repositories/IUserRepository.ts
+++ src/api/modules/usuario/typeorm/repositories/IUserRepository.ts
@@ -1,6 +1,11 @@
 import UserRequestDTO from "../../dto/UserRequestDTO";
 import UserResponseDTO from "../../dto/UserResponseDTO";
+import Usuario from "../entities/Usuario";
 
 export default interface IUserRepository{
-    createUser(userRequest: UserRequestDTO):Promise<UserResponseDTO | undefined>
+    createUser(userRequest: Usuario):Promise<Usuario | undefined>
+    updateUser(userRequest: Usuario): Promise<Usuario | undefined>
+    deleteUser(id:number): Promise<void>
+    findUserById(id:number): Promise<Usuario | null>
+    finAllUsers():Promise<Usuario[] | null>
 }
