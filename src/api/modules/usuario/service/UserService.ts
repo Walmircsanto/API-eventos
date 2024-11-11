df12677b9bb5f4d55e39e034e771ef34ab93901b
Walmircsanto <walmirsantosrosa@gmail.com>
2024-11-11 10:33:55 -0300

    :sparkles: feat(UserService): create crud methods in the service

---
+++ src/api/modules/usuario/service/UserService.ts
@@ -0,0 +1,80 @@
+import {inject, injectable} from "tsyringe";
+import UsersRepository from "../typeorm/repositories/UserRepository";
+import UserRequestDTO from "../dto/UserRequestDTO";
+import UserMapper from "../mapper/UserMapper";
+import AppError from "../../../shared/errors/AppError";
+
+@injectable()
+export default class UserService {
+
+    // definimos como read only para ser imutavel.
+    constructor(@inject(UsersRepository) private readonly usersRepository: UsersRepository) {
+
+    }
+
+    public async createUser(userRequest: UserRequestDTO) {
+
+        const emailExist = await this.usersRepository.existEmail(userRequest.email)
+
+
+        if (emailExist) {
+            throw new AppError("Email already exist", "Bad Request");
+        }
+        const user = await this.usersRepository.createUser(UserMapper.parseRequestDTOInUserEntity(userRequest));
+
+        return user;
+    }
+
+
+    public async findUserById(id: number) {
+        const user = await this.usersRepository.findUserById(id);
+
+
+        if (!user) {
+            throw new AppError("User not found", "Bad request");
+        }
+
+        return user;
+    }
+
+    public async deleteUser(id: number) {
+        const user = this.usersRepository.findUserById(id);
+
+        if (!user) {
+            throw new AppError("User not found", "Bad request");
+        }
+        await this.usersRepository.deleteUser(id);
+    }
+
+    public async updateUser(userRequest: UserRequestDTO) {
+        const idUser = userRequest.id;
+        if (idUser) {
+            const user = await this.usersRepository.findUserById(idUser);
+
+            console.log(user)
+            if (user != null) {
+
+                user.id = idUser;
+                user.nome = userRequest.nome;
+                user.email = userRequest.email;
+                user.password = userRequest.password;
+                user.idade = userRequest.idade;
+                user.categoria = userRequest.categoria;
+                return await this.usersRepository.updateUser(user);
+
+            }
+            throw new AppError("User not found", "Bad request");
+
+
+        }
+    }
+
+    public async finAllUser() {
+        const users = await this.usersRepository.finAllUsers();
+        if (!users) {
+            throw new AppError("List Users not found", "Bad request");
+        }
+        return users;
+    }
+
+}
