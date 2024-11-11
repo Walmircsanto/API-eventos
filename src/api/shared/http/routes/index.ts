baa5652e856ba011f4f6b922ef372d01510a27e0
Walmircsanto <walmirsantosrosa@gmail.com>
2024-11-11 10:36:51 -0300

    :sparkles: feat(routes/index.ts): add route for user

--- src/api/shared/http/routes/index.ts
+++ src/api/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import eventosRouter from "../../../modules/eventos/routes/EventosRouter";
+import userRoutes from "../../../modules/usuario/routes/UserRoutes";
 
 const routes = Router();
 
 routes.use('/eventos',eventosRouter)
+routes.use('/user',userRoutes)
 
 export default routes;
