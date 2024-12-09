93957cb7b6d23c843c2b89e864b9089e1c77350c
Walmircsanto <walmirsantosrosa@gmail.com>
2024-12-09 17:35:08 -0300

    :bug: fix(Auth.ts): fix enviroment variables

--- src/api/config/Auth.ts
+++ src/api/config/Auth.ts
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 import * as process from "node:process";
 dotenv.config();
 export const auth = {
-    secret:  String(process.env.JWT_SECRET),
-    expiresIn: String(process.env.JWT_EXPIRE_IN),
+    secret:  String(process.env.SECRECT),
+    expiresIn: String(process.env.EXPIRE_IN),
 }
