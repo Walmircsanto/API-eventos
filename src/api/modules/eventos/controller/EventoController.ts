f648cfd340a10ea1e02e117a8b6deb331ab85a0d
Walmircsanto <walmirsantosrosa@gmail.com>
2025-01-09 09:27:14 -0300

    :sparkles: feat: added the updateEvent method and fixed the vacancy number

--- src/api/modules/eventos/controller/EventoController.ts
+++ src/api/modules/eventos/controller/EventoController.ts
@@ -1,70 +1,106 @@
 import "reflect-metadata";
-import { EventoService } from "../services/EventoService";
-import { Request, Response } from "express";
-import { inject, injectable } from "tsyringe";
+import {EventoService} from "../services/EventoService";
+import {Request, Response} from "express";
+import {inject, injectable} from "tsyringe";
 import EventoRequest from "../dto/EventoRequest";
-import { CONSTRAINT } from "sqlite3";
-import AppError from "../../../shared/errors/AppError";
 
 @injectable()
 export default class EventoController {
-  constructor(
-    @inject(EventoService) private readonly eventoService: EventoService
-  ) {}
+    constructor(
+        @inject(EventoService) private readonly eventoService: EventoService
+    ) {
+    }
 
-  // @ts-ignore
-  public async createEvento(req: Request, res: Response): Promise<Response | undefined> {
-    const {
-      titulo,
-      img,
-      status,
-      descricao,
-      dataInicio,
-      dataFim,
-      usuariosIds,
-      certificadoId,
-    } = req.body;
+    // @ts-ignore
+    public async createEvento(req: Request, res: Response): Promise<Response | undefined> {
+        const {
+            titulo,
+            img,
+            status,
+            descricao,
+            dataInicio,
+            dataFim,
+            numVagas,
+            usuariosIds,
+            certificadoId,
+        } = req.body;
 
-    const fileName = req.file?.filename;
-    const event = await this.eventoService
-      .createEvent({
-        titulo,
-        img: fileName || img,
-        status,
-        descricao,
-        dataInicio,
-        dataFim,
-        usuariosIds,
-        certificadoId,
-      })
-      .then((event) => {
+        // se num vagas tiver algo então converte em um inteiro, caso contrario ele undefined
+        const parsedNumVagas = numVagas ? parseInt(numVagas, 10) : null;
+
+        if (numVagas && isNaN(<number>parsedNumVagas)) {
+            return res.status(400).json({ error: 'numVagas deve ser um número válido' });
+        }
+        const fileName = req.file?.filename;
+        const event = await this.eventoService
+            .createEvent(<EventoRequest>{
+                titulo,
+                img: fileName || img,
+                status,
+                descricao,
+                dataInicio,
+                dataFim,
+                numVagas: parsedNumVagas,
+                usuariosIds,
+                certificadoId,
+            })
+
+        return res.status(201).json(event);
+    }
+
+    public async findById(
+        req: Request,
+        res: Response
+    ): Promise<Response | undefined> {
+        const id = parseInt(req.params.id);
+
+        const event = await this.eventoService.findById(id);
+
         return res.status(200).json(event);
-      });
-  }
+    }
 
-  public async findById(
-    req: Request,
-    res: Response
-  ): Promise<Response | undefined> {
-    const id = parseInt(req.params.id);
+    public async findAll(req: Request, res: Response) {
+        const eventos = await this.eventoService.findAllEventos();
 
-    const event = await this.eventoService.findById({ id });
+        return res.status(200).json(eventos);
+    }
 
-    return res.status(200).json(event);
-  }
+    public async deleteEvento(req: Request, res: Response) {
+        const id = parseInt(req.params.id);
+        await this.eventoService.deleteEvento(id);
 
-  public async findAll(req: Request, res: Response) {
+        return res.status(204).json("");
+    }
 
-    console.log("CHAMOU EVENTOSERVICE");
-    const eventos = await this.eventoService.findAllEventos();
+    public async updateEvento(req: Request, res: Response) {
+        const {
+            id,
+            titulo,
+            img,
+            status,
+            descricao,
+            dataInicio,
+            dataFim,
+            numVagas,
+            usuariosIds,
+            certificadoId
+        } = req.body
 
-    return res.status(200).json(eventos);
-  }
+        const event = await this.eventoService.updateEvento({
+            id,
+            titulo,
+            img,
+            status,
+            descricao,
+            dataInicio,
+            dataFim,
+            numVagas,
+            usuariosIds,
+            certificadoId
+        });
 
-  public async deleteEvento(req: Request, res: Response) {
-    const id = parseInt(req.params.id);
-    await this.eventoService.deleteEvento(id);
+        return res.status(200).json(event);
 
-    return res.status(204).json("");
-  }
+    }
+
 }
