0c6226d7cbef311c6ff59c037d3f7f5ca66529cf
Walmircsanto <walmirsantosrosa@gmail.com>
2025-02-12 10:57:50 -0300

    :sparkles: feat: Added the redis application

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,15 @@ import 'express-async-errors';
 import {AppDataSource} from './api/shared/typeorm/data-source'
 import routes from "./api/shared/http/routes";
 import AppError from "@modules/errors/AppError";
+import * as redis from 'redis';
 
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 app.use('/api', routes); // estou indicando que meu servidor vai usar essas rotas
 
@@ -28,6 +31,23 @@ app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     });
 })
 
+
+export const BlackListedRedisClient = redis.createClient({
+    url:'redis://localhost:6379',
+    password:'',
+});
+
+BlackListedRedisClient.on('connect',()=>{
+    console.log(`Redis running on: localhost 6379`)
+})
+
+BlackListedRedisClient.on("error", (err:Error)=>{
+    console.log(err);
+})
+
+
+
+
 //inicializar o banco de dados
 AppDataSource.initialize().then(r => {
     console.log("data source initialized");
@@ -35,6 +55,11 @@ AppDataSource.initialize().then(r => {
     console.log(err);
 });
 
-app.listen(3030, () => {
-    console.log("Server is running on port 3000");
-})
+const init = async()=>{
+    await BlackListedRedisClient.connect()
+    app.listen(3030, () => {
+        console.log(`server listening on port: ${3030}`)
+    })
+}
+
+init()
