52751383c3425fb5f248a2a89a9573327a4d2156
Walmircsanto <walmirsantosrosa@gmail.com>
2025-02-12 20:18:37 -0300

    :sparkles: feat: Added email sending and caching method with redis

--- src/api/modules/usuario/service/EventSubscribe.ts
+++ src/api/modules/usuario/service/EventSubscribe.ts
@@ -4,6 +4,8 @@ import {EventoService} from "../../eventos/services/EventoService";
 import AppError from "@modules/errors/AppError";
 import EventoRepository from "../../eventos/typeorm/repositories/EventoRepository";
 import send from "../../email/service/SendEmail";
+import Evento from "../../eventos/typeorm/entities/Evento";
+import {BlackListedRedisClient} from "@config/redisConfig";
 
 
 @injectable()
@@ -19,16 +21,20 @@ export default class EventSubscribe {
         const user = await this.userService.findUserById(idUser);
         //busco um evento pelo ID
         const event = await this.eventoRepository.findEventoById(idEvent)
-        if(!event) throw  new AppError("Event Not found", "Bad Request")
+        if (!event) throw new AppError("Event Not found", "Bad Request")
 
-        if(!user) throw  new AppError("User Not Found", "Bad Request")
+        if (!user) throw new AppError("User Not Found", "Bad Request")
 
         if (event.numVagas > 0) {
             event.usuarios = event.usuarios || [];
             event.usuarios.push(user);
             event.numVagas--;
 
-            const evento =  await this.eventoRepository.updateEventoEntity(event);
+            const evento = await this.eventoRepository.updateEventoEntity(event);
+            this.addEventRedis(evento);
+            send(user.email, 'inscrição no Evento', `Parabens por se increver no evento, 
+            que acontecera entre ${evento.dataInicio.toUTCString()} e ${evento.dataFim.toString()}`)
+
 
             return evento
         } else {
@@ -36,5 +42,17 @@ export default class EventSubscribe {
         }
 
 
+    }
+
+    private async addEventRedis(evento: Evento): Promise<void> {
+        await BlackListedRedisClient.hSet('eventos' + evento.id, {
+            'id': evento.id.toString(),
+            'titulo': evento.titulo,
+            'img': evento.img,
+            'status': evento.status.toString(),
+            'descricao': evento.descricao,
+            'dataInicio': evento.dataInicio.toUTCString(),
+            'dataFim': evento.dataFim.toUTCString()
+        });
     }
 }
