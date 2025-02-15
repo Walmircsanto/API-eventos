16ea35cff9ff34b6806a529c17c6848d9603911a
Walmircsanto <walmirsantosrosa@gmail.com>
2025-02-15 11:04:46 -0300

    :sparkles: feat: Created service to fetch events from a user

--- src/api/modules/usuario/service/UserEvents.ts
+++ src/api/modules/usuario/service/UserEvents.ts
@@ -1,1 +1,12 @@
-export default class 
+import {inject, injectable} from "tsyringe";
+import EventoRepository from "../../eventos/typeorm/repositories/EventoRepository";
+
+@injectable()
+export default class UserEvents {
+    constructor(@inject(EventoRepository) private eventoRepository: EventoRepository) {
+    }
+
+    public async findEventsUser(id:number){
+       return await this.eventoRepository.findEventsUser(id)
+    }
+}
