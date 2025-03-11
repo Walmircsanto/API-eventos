4ee3af1d3699cd10a6312c090be62a645b582479
Walmircsanto <walmirsantosrosa@gmail.com>
2025-03-11 15:47:03 -0300

    :sparkles: feat: Created mapper container

--- src/api/containes/containerUser.ts
+++ src/api/containes/containerUser.ts
@@ -1,19 +1,10 @@
 import {container} from "tsyringe";
 
-import EventoRepository from "../modules/eventos/typeorm/repositories/EventoRepository";
-import {EventoService} from "../modules/eventos/services/EventoService";
-import EventoController from "../modules/eventos/controller/EventoController";
 import UsersRepository from "../modules/usuario/typeorm/repositories/UserRepository";
 import UserService from "../modules/usuario/service/UserService";
 import UserController from "../modules/usuario/controller/UserController";
 import EventSubscribe from "../modules/usuario/service/EventSubscribe";
 import UserEvents from "../modules/usuario/service/UserEvents";
-
-
-//basicamente ta dizendo que quando precisar instancia ou usar uma instancia do eventoRepository e esse containes que vai gerenciar
-container.registerSingleton(EventoRepository);
-container.registerSingleton(EventoService);
-container.registerSingleton(EventoController);
 
 container.registerSingleton(UserController);
 
